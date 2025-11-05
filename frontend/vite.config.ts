d6bd8199e954fabc14616ee229a89c58f9ad88a5
Florian Stemmler <florian.stemmler@outlook.de>
2025-11-05 23:11:31 +0100

    Oxlint hinzugefügt und konfiguriert, Vitest mit Browser Mode und React Rendering initialisiert

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,23 @@
+/// <reference types="vitest/config" />
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    !process.env.VITEST && reactRouter(),
+    tsconfigPaths(),
+  ],
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright(),
+      // pnpm exec playwright install
+      instances: [{ browser: 'chromium' }, { browser: 'firefox' }],
+    },
+  },
 });
