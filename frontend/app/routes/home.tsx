5babc900dbe3807caf3ed50a3a305beec524fc87
Huyen Bao Han Nguyen <s87044@informatik.htw-dresden.de>
2025-11-13 22:58:18 +0100

    feat(register): create welcome modal

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,14 @@
-import { Welcome } from '../components/welcome/welcome';
-import type { Route } from './+types/home';
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ];
-}
+import { useState } from "react";
+import Welcome from '../components/welcome/welcome';
 
 export default function Home() {
-  return <Welcome />;
+  const [showWelcome, setShowWelcome] = useState(true); // mặc định hiển thị modal
+
+  return (
+    <div>
+      <Welcome open={showWelcome} onClose={() => setShowWelcome(false)} />
+      <h1 className="text-2xl font-bold mt-4">Home Page Content</h1>
+      <p>Hier kommt der restliche Inhalt deiner Homepage.</p>
+    </div>
+  );
 }
