07081dd16c26cae591df340fe4af59c000038cf6
Moritz Stötzner <moritz.stoetzner@gmx.de>
2026-01-22 16:11:51 +0100

    wagon router

--- src/Backend/app/main.py
+++ src/Backend/app/main.py
@@ -4,7 +4,7 @@ load_dotenv(find_dotenv())
 import os
 from app.seed import seed_demo_data
 from contextlib import asynccontextmanager
-from app.router import userRouter, routeRouter, vehicleRouter, companyRouter
+from app.router import userRouter, routeRouter, vehicleRouter, companyRouter, wagonRouter
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app import database
@@ -16,7 +16,7 @@ async def lifespan(app: FastAPI):
     print("DB initialized")
 
     # Generating test data
-    if os.getenv("SEED_DEMO_DATA", "true").lower() == "true":
+    if os.getenv("SEED_DEMO_DATA", "false").lower() == "true":
         seed_demo_data(database.get_db())  # pass session generator
 
     # continue to application
@@ -31,3 +31,4 @@ app.include_router(userRouter.router)
 app.include_router(vehicleRouter.router)
 app.include_router(routeRouter.router)
 app.include_router(companyRouter.router)
+app.include_router(wagonRouter.router)
