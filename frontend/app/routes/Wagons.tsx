7aa17555ef66d7b7e1677c21d84f01af202ddb22
Florian Stemmler <florian.stemmler@outlook.de>
2026-02-23 23:07:42 +0100

    loader von Wagons Route entfernt, Wagon Datentyp bei optionale Daten null entfernt

--- frontend/app/routes/Wagons.tsx
+++ frontend/app/routes/Wagons.tsx
@@ -1,78 +1,73 @@
-import { useLoaderData } from "react-router";
-import type { Route } from "../+types/root";
-import { API_BASE_URL } from "@/services/api"
-import { getWagons } from "@/services/wagons";
+import { useLoaderData } from 'react-router';
 
+import type { Route } from './+types/Wagons';
+import { API_BASE_URL } from '@/services/api';
+import { getWagons } from '@/services/wagons';
 
-export async function loader({ }: Route.LoaderArgs) {
-  return { trassen: [] };
-}
-
-export async function clientLoader({ }: Route.ClientLoaderArgs) {
-
+export async function clientLoader({}: Route.ClientLoaderArgs) {
   const trassen = await getWagons();
   return { trassen };
 }
-clientLoader.hydrate = true as const;
 
 export default function Wagons() {
-
   const wagons = useLoaderData<typeof clientLoader>();
 
   return (
-    <div className="flex flex-col min-w-250">
+    <div className="flex min-w-250 flex-col">
       <div>
-        <h1 className="text-3xl font-bold mb-4">Wagons</h1>
+        <h1 className="mb-4 text-3xl font-bold">Wagons</h1>
       </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center"> {
-        wagons.trassen.map((wagon) => {
+      <div className="flex flex-col items-center justify-center gap-4">
+        {' '}
+        {wagons.trassen.map((wagon) => {
           const imagePath = `${API_BASE_URL}/static/${wagon.img_file}`;
           return (
-            <div className="flex flex-col gap-2 bg-[#121C27] rounded-xl min-w-250 px-5 pt-2 pb-5">
-              <div className="text-lg italic text-[#D5DCE3]">
+            <div
+              key={wagon.id}
+              className="flex min-w-250 flex-col gap-2 rounded-xl bg-[#121C27] px-5 pt-2 pb-5"
+            >
+              <div className="text-lg text-[#D5DCE3] italic">
                 {wagon.vehicle_number}
               </div>
-              <div className="flex flex-row gap-5 justify-between">
+              <div className="flex flex-row justify-between gap-5">
                 <div>
                   <img src={imagePath} alt="Wagon" />
                 </div>
-                <div className="flex flex-col gap-1 justify-between w-90">
+                <div className="flex w-90 flex-col justify-between gap-1">
                   <div className="flex rounded border border-[#223041] bg-[#101822] px-5 py-1">
-                    <div className="text-sm w-50">Höchstgeschwindigkeit</div>
+                    <div className="w-50 text-sm">Höchstgeschwindigkeit</div>
                     <div className="text-sm">{wagon.max_speed} km/h</div>
                   </div>
                   <div className="flex rounded border border-[#223041] bg-[#101822] px-5 py-1">
-                    <div className="text-sm w-50">Neupreis Standardwagen</div>
+                    <div className="w-50 text-sm">Neupreis Standardwagen</div>
                     <div className="text-sm">{wagon.price_standard} €</div>
                   </div>
                   <div className="flex rounded border border-[#223041] bg-[#101822] px-5 py-1">
-                    <div className="text-sm w-50"> Neupreis Steuerwagen</div>
+                    <div className="w-50 text-sm"> Neupreis Steuerwagen</div>
                     <div className="text-sm">{wagon.price_control} €</div>
                   </div>
                   <div className="flex rounded border border-[#223041] bg-[#101822] px-5 py-1">
-                    <div className="text-sm w-50"> Kilometerkosten (alle Arten)</div>
+                    <div className="w-50 text-sm">
+                       Kilometerkosten (alle Arten)
+                    </div>
                     <div className="text-sm">{wagon.cost_km! / 100} €/km</div>
                   </div>
                   <div className="flex rounded border border-[#223041] bg-[#101822] px-5 py-1">
-                    <div className="text-sm w-50"> Kapazität (alle Arten)</div>
+                    <div className="w-50 text-sm"> Kapazität (alle Arten)</div>
                     <div className="text-sm">{wagon.capacity} Personen</div>
                   </div>
-
-
                 </div>
-                <div className="flex justify-center items-center w-60">
-                  <button className="rounded-xl px-5 py-1 cursor-pointer bg-[#15467D]">leasen</button>
+                <div className="flex w-60 items-center justify-center">
+                  <button className="cursor-pointer rounded-xl bg-[#15467D] px-5 py-1">
+                    leasen
+                  </button>
                 </div>
-
               </div>
-
             </div>
-          )
-        })
-      }</div>
-
+          );
+        })}
+      </div>
     </div>
   );
-
 }
--- frontend/app/services/wagons.ts
+++ frontend/app/services/wagons.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "./api";
+import { API_BASE_URL } from './api';
 
 export interface Wagon {
   id: number;
@@ -6,28 +6,26 @@ export interface Wagon {
   owner_company_id: number;
   type_id: number;
   acquired_at: string; // ISO date string
-  leasing_model: string | null;
-  lease_annual_rate_percent: number | null;
-  lease_weekly_rate_percent: number | null;
-  lease_start: string | null; // ISO date string or null
+  leasing_model?: string;
+  lease_annual_rate_percent?: number;
+  lease_weekly_rate_percent?: number;
+  lease_start?: string; // optional ISO date string
   condition_percent: number;
   is_leased: boolean;
   img_file: string;
 
-  max_speed?: number;        // km/h
-  price_standard?: number;   // standard price
-  price_control?: number;    // controlled/adjusted price
-  cost_km?: number;          // cost per km
+  max_speed?: number; // km/h
+  price_standard?: number; // standard price
+  price_control?: number; // controlled/adjusted price
+  cost_km?: number; // cost per km
   capacity?: number;
 }
 
-
 export async function getWagons() {
-
   const res = await fetch(`${API_BASE_URL}/wagons`, { credentials: 'include' });
 
   if (!res.ok) {
-    throw new Error(JSON.stringify(res))
+    throw new Error(JSON.stringify(res));
   }
 
   return res.json() as Promise<Wagon[]>;
